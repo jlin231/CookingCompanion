376b5337469164b460811f5861bae5d11fab79cc
jlin231 <jonlin981@gmail.com>
2023-03-10 15:31:59 -0800

    added comments

--- react-app/src/components/SingleRecipePage/EditCommentCard/index.js
+++ react-app/src/components/SingleRecipePage/EditCommentCard/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import SingleRecipePage from '..';
+import { useDispatch} from 'react-redux';
 import { thunkEditCommentOfRecipe, thunkDeleteCommentRecipe, thunkGetSingleRecipe } from '../../../store/recipe';
 
 function CommentCard({ comment, recipeId }) {
